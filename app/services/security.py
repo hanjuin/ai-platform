5446f3d34d896f793eaa81d0a13b2f2bf83e98fa
hjwong97 <hjwong12@gmail.com>
2026-03-31 12:31:31 +1300

    chore: clean code, add readme and .env.example

--- app/services/security.py
+++ app/services/security.py
@@ -1,15 +1,14 @@
 from passlib.context import CryptContext
-from jose import jwt
 from datetime import datetime, timedelta
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from jose import JWTError, jwt
-
+import os
 from app.dependencies import get_db
 from app.models.db_models import User, UserRole
 
-SECRECT_KEY = "supersecretkey"
+SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -27,14 +26,14 @@ def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode,SECRECT_KEY, algorithm=ALGORITHM)
+    return jwt.encode(to_encode,SECRET_KEY, algorithm=ALGORITHM)
 
 def get_current_user(
         token: str = Depends(oauth2_scheme),
         db: Session = Depends(get_db)
         ):
     try:
-        payload = jwt.decode(token, SECRECT_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise HTTPException(status_code=401, detail="Invalid Token")
